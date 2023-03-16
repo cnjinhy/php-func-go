0123d64c1ba9f8422d722b819d05b6af9f3263be
jinhongyu <jinhongyu@licaimofang.com>
2023-03-16 19:00:34 +0800

    Update

---
+++ tests/checkdate_test.go
@@ -0,0 +1,26 @@
+package tests
+
+import (
+	"phpfunc/php"
+	"testing"
+)
+
+func TestCheckdate(t *testing.T) {
+	result := php.CheckDate(12, 31, 2000)
+	expected := true
+	if result != expected {
+		t.Errorf("Checkdate(12, 31, 2000) returned %t, expected %t", result, expected)
+	}
+
+	result = php.CheckDate(2, 29, 2001)
+	expected = false
+	if result != expected {
+		t.Errorf("Checkdate(2, 29, 2000) returned %t, expected %t", result, expected)
+	}
+
+	result = php.CheckDate(12, 31, -400)
+	expected = false
+	if result != expected {
+		t.Errorf("Checkdate(12, 31, -400) returned %t, expected %t", result, expected)
+	}
+}
