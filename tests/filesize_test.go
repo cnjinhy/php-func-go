1250513cdeefb767382158479b4ea9237d4d4add
jinhongyu <jinhongyu@licaimofang.com>
2023-03-18 10:59:53 +0800

    filesize

---
+++ tests/filesize_test.go
@@ -0,0 +1,14 @@
+package tests
+
+import (
+	"phpfunc/php"
+	"testing"
+)
+
+func TestFilesize(t *testing.T) {
+	result, _ := php.Filesize(php.Dirname(php.MAGIC__FILE__()) + "/test.txt")
+	expected := int64(7)
+	if result != expected {
+		t.Errorf("Filesize("+php.Dirname(php.MAGIC__FILE__())+"/test.txt"+") returned %d, expected %d", result, expected)
+	}
+}
