e1503134e2679ddd4b357330d62c4babb71e0ce3
jinhongyu <jinhongyu@licaimofang.com>
2023-04-01 22:32:29 +0800

    Update func empty

---
+++ php/empty.go
@@ -0,0 +1,64 @@
+package php
+
+import "reflect"
+
+func Empty(val interface{}) bool {
+	if val == nil {
+		return true
+	}
+	var (
+		int0   int   = 0
+		int80  int8  = 0
+		int160 int16 = 0
+		int320 int32 = 0
+		int640 int64 = 0
+
+		uint0   uint   = 0
+		uint80  uint8  = 0
+		uint160 uint16 = 0
+		uint320 uint32 = 0
+		uint640 uint64 = 0
+
+		float320 float32 = 0
+		float640 float32 = 0
+	)
+
+	switch reflect.TypeOf(val).Kind() {
+	case reflect.Slice, reflect.Array, reflect.Map:
+		return reflect.ValueOf(val).Len() == 0
+	case reflect.String:
+		return len(val.(string)) == 0 || val.(string) == "0"
+	case reflect.Ptr:
+		return Empty(reflect.ValueOf(val).Elem().Interface())
+	case reflect.Struct:
+		return reflect.DeepEqual(val, reflect.Zero(reflect.TypeOf(val)).Interface())
+	case reflect.Bool:
+		return !val.(bool)
+	case reflect.Int:
+		return val == int0
+	case reflect.Int8:
+		return val == int80
+	case reflect.Int16:
+		return val == int160
+	case reflect.Int32:
+		return val == int320
+	case reflect.Int64:
+		return val == int640
+	case reflect.Uint:
+		return val == uint0
+	case reflect.Uint8:
+		return val == uint80
+	case reflect.Uint16:
+		return val == uint160
+	case reflect.Uint32:
+		return val == uint320
+	case reflect.Uint64:
+		return val == uint640
+	case reflect.Float32:
+		return val == float320
+	case reflect.Float64:
+		return val == float640
+	default:
+		return reflect.ValueOf(val).IsZero()
+	}
+}
---
+++ tests/empty_test.go
@@ -0,0 +1,144 @@
+package tests
+
+import (
+	"github.com/cnjinhy/php-func-go/php"
+	"testing"
+)
+
+func TestEmpty(t *testing.T) {
+	var test1 int
+	test1 = 1
+	result := php.Empty(test1)
+	expected := false
+	if result != expected {
+		t.Errorf("TestEmpty(test1) returned %t, expected %t", result, expected)
+	}
+
+	var test2 int64
+	test2 = 0
+	result = php.Empty(test2)
+	//php.VarDump(result)
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test2) returned %t, expected %t", result, expected)
+	}
+
+	var test21 int
+	test21 = 0
+	result = php.Empty(test21)
+	//php.VarDump(result)
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test21) returned %t, expected %t", result, expected)
+	}
+
+	var test22 int64
+	test22 = 1
+	result = php.Empty(test22)
+	//php.VarDump(result)
+	expected = false
+	if result != expected {
+		t.Errorf("TestEmpty(test22) returned %t, expected %t", result, expected)
+	}
+
+	test3 := ""
+	result = php.Empty(test3)
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test3) returned %t, expected %t", result, expected)
+	}
+
+	test4 := "0"
+	result = php.Empty(test4)
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test4) returned %t, expected %t", result, expected)
+	}
+
+	test5 := false
+	result = php.Empty(test5)
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test5) returned %t, expected %t", result, expected)
+	}
+
+	test6 := true
+	result = php.Empty(test6)
+	expected = false
+	if result != expected {
+		t.Errorf("TestEmpty(test6) returned %t, expected %t", result, expected)
+	}
+
+	test7 := "01"
+	result = php.Empty(test7)
+	expected = false
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	test8 := make(map[string]interface{})
+	test8["a"] = "ValueA"
+	result = php.Empty(test8)
+	expected = false
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	test81 := make(map[string]interface{})
+	test81["a"] = "ValueA"
+	result = php.Empty(test81["b"])
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	test82 := make(map[string]interface{})
+	test82["a"] = "ValueA"
+	result = php.Empty(test81["b"])
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	test83 := make(map[string]map[string]int)
+	test83["a"] = make(map[string]int)
+	test83["a"]["s"] = 1
+	test83["a"]["ss"] = 0
+	result = php.Empty(test83["b"])
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	result = php.Empty(test83["a"])
+	expected = false
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	result = php.Empty(test83["a"]["t"])
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	result = php.Empty(test83["a"]["s"])
+	expected = false
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	result = php.Empty(test83["a"]["ss"])
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+	test9 := make(map[string]interface{})
+	result = php.Empty(test9)
+	expected = true
+	if result != expected {
+		t.Errorf("TestEmpty(test7) returned %t, expected %t", result, expected)
+	}
+
+}
